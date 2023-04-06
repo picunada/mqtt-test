75276c3e185c9dbb5dc5e7c67fec07e2a018086a
Daniil Bezuglov <xlaystgoku@gmail.com>
2023-04-06 22:16:06 +0700

    Added interface

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,53 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+  let power: number = 50
+</script>
+
+<main id="container">
+  <div id="content">
+    <h1>Мощность:</h1>
+    <h2>{power}%</h2>
+    <div id="slider-container">
+      <input type="range" min="1" max="100" bind:value={power} class="slider" id="myRange" />
+    </div>
+  </div>
+</main>
+
+<style scoped>
+  * {
+    margin: 0;
+  }
+
+  h1 {
+    text-transform: capitalize;
+    font-weight: 700;
+  }
+
+  h2 {
+    font-size: 50px;
+  }
+
+  #container {
+    width: 100vw;
+    height: 100vh;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+
+  #content {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  }
+
+  #slider-container {
+    width: 100%;
+  }
+
+  .slider {
+    width: 100%; /* Full-width */
+    height: 25px; /* Specified height */
+    -webkit-transition: 0.2s; /* 0.2 seconds transition on hover */
+    transition: opacity 0.2s;
+  }
+</style>
